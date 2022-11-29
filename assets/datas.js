7ccb385f031d829f269b8ce75691e68050d181a7
archist <mentor03.archist@gmail.com>
2022-11-30 00:26:02 +0900

    UPD fichier assets/data.js 1ere implémentation

--- assets/datas.js
+++ assets/datas.js
@@ -1,15 +1,53 @@
 const jsonDatas = {
-    P1C2E1: {
-        h: "....Titre de l'exercice...."
-        , p: `....Préambule de l'exercice....`
-        , sol: ""
-        , tasks: {
-            p: `....Énoncé de l'exercice....`
-            , ol: [
-                `
-                  ....TODOList tasks....
-                `
-            ]
-        }
+    environnment1: {
+        h: "Mettez en place votre environnement de développement"
+    }
+    ,
+    environnment2: {
+        h: "ENV1: Écrivez du javascript pour le web activity"
+    }
+    ,
+    pratiquez1: {
+        h: "Pratiquez 1: Ma passion pour les animaux"
+    }
+    ,
+    pratiquez2: {
+        h: "Pratiquez 2: Les cupcakes"
+    }
+    ,
+    pratiquez3: {
+        h: "Pratiquez 3: Le blog trotter"
+    }
+    ,
+    cv1: {
+        h: "CV1: Entraînez-vous en structurant votre CV"
+    }
+    ,
+    cv2: {
+        h: "CV2: Entraînez-vous en mettant en forme votre CV"
+    }
+    ,
+    cv3: {
+        h: "CV3: Entraînez-vous en organisant votre CV"
+    }
+    ,
+    cv4: {
+        h: "CV4: Entraînez-vous en adaptant votre CV en responsive"
+    }
+    ,
+    TP: {
+        h: "TP : Créez un site pas à pas"
+    }
+    ,
+    webinaire1: {
+        h: "Webinaire: Découper une maquette pour préparer l'intégration"
+    }
+    ,
+    webinaire2: {
+        h: "Webinaire: Intégrer une maquette"
+    }
+    ,
+    defi: {
+        h: "Défi: Intégrer une maquette"
     }
 }
